b14f04c4a9ba34781e2c95f1584a391294fcbdd1
Meg Grasse <meggrasse@gmail.com>
2016-03-04 13:17:13 -0600

    password secret

--- messaging.js
+++ messaging.js
@@ -1,4 +1,5 @@
 function send(){
     var userinput = document.getElementById("rawinput").value;
     document.getElementById("message2").innerHTML = userinput;
+    document.getElementById("rawinput").value = '';
 }
