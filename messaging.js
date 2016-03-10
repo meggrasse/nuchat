cfd0f96efa44922f6b3a8ed56375d509e1fa92d1
Meg Grasse <meggrasse@gmail.com>
2016-03-09 18:11:08 -0600

    new but not perfect functionality

--- messaging.js
+++ messaging.js
@@ -1,5 +1,48 @@
-function send(){
+function send2(){
     var userinput = document.getElementById("rawinput").value;
     document.getElementById("message2").innerHTML = userinput;
-    document.getElementById("rawinput").value = '';
+}
+
+function send4(){
+    var userinput = document.getElementById("rawinput").value;
+    document.getElementById("message4").innerHTML = userinput;
+}
+
+function send6(){
+    var userinput = document.getElementById("rawinput").value;
+    document.getElementById("message6").innerHTML = userinput;
+}
+
+function send8(){
+    var userinput = document.getElementById("rawinput").value;
+    document.getElementById("message8").innerHTML = userinput;
+}
+
+function send10(){
+    var userinput = document.getElementById("rawinput").value;
+    document.getElementById("message10").innerHTML = userinput;
+}
+
+function send12(){
+    var userinput = document.getElementById("rawinput").value;
+    document.getElementById("message12").innerHTML = userinput;
+}
+
+function send14(){
+    var userinput = document.getElementById("rawinput").value;
+    document.getElementById("message14").innerHTML = userinput;
+}
+
+function send16(){
+    var userinput = document.getElementById("rawinput").value;
+    document.getElementById("message16").innerHTML = userinput;
+}
+
+function send18(){
+    var userinput = document.getElementById("rawinput").value;
+    document.getElementById("message18").innerHTML = userinput;
+}
+
+function clear() {
+	document.getElementById("rawinput").value = '';
 }
